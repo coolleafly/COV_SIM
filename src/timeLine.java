f99653ff95e55a814190042c4e3ab80a28381c0b
coolleafly <reg@kuye.cn>
2021-03-06 18:55:26 +0800

    Update timeLine.java

--- src/timeLine.java
+++ src/timeLine.java
@@ -2,19 +2,19 @@ public class timeLine
 {
     public static void timerun()
     {
-//        if(MyPanel.worldTime/10.0==10)                               //——————2020.02.14更改
+//        if(MyPanel.worldTime/10.0==10)                               //test in 20200214
 //        {
 //            Constants.BROAD_RATE=0.8f;
 //            Constants.FATALITY_RATE =0.05f;
 //            Constants.FLOW=0.99f;
 //        }
-//        if(MyPanel.worldTime/10.0==20)                             //——————2020.02.14更改
+//        if(MyPanel.worldTime/10.0==20)                            //test in 20200214
 //        {
 //            Constants.BROAD_RATE=0.8f;
 //            Constants.FATALITY_RATE =0.05f;
 //            Constants.FLOW=0.99f;
 //        }
-//        if(MyPanel.worldTime/10.0==50)                            //——————2020.02.14更改
+//        if(MyPanel.worldTime/10.0==50)                         //test in 20200214
 //        {
 //            Constants.BROAD_RATE=0.8f;
 //            Constants.FATALITY_RATE =0.05f;
